82c48d81ad39601916bb945ce89df484214b08ce
amberhaf <amberhaf@hotmail.com>
2020-09-27 18:01:43 +0100

    Firsttry

---
+++ client/build/precache-manifest.88a85950d4834babd540a22aa53e9169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a394a9076a628374a02ef280ceb1cec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ef04c525ddab1d0f587",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "aab613ce03b20e60e13c",
+    "url": "/static/js/2.75867594.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.75867594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef04c525ddab1d0f587",
+    "url": "/static/js/main.ff80a9be.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
