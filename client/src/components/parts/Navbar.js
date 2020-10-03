2c250b362a4d12ef2e8e32d25c8b08da8be6f8b5
amberhaf <amberhaf@hotmail.com>
2020-10-03 23:13:39 +0100

    My first commit

--- client/src/components/parts/Navbar.js
+++ client/src/components/parts/Navbar.js
@@ -4,21 +4,23 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-secondary fixed-top" style={{backgroundColor: "#071740", position: "sticky"}} id="mainNav">
-            <div className="container">
-        <div className="text-center">    <img className="logo3" src="../logo.svg" alt="" /><a class="brand-logo navbar-brand js-scroll-trigger" href="#page-top"><a class="navName brand-logo">MU Women In Stem</a></a>
-    <a className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-    <img className="menu" src="../bars.svg" alt="" /></a></div>
-                <div className="collapse navbar-collapse navbar-expand" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
-                    <li className="nav-item mx-0 mx-lg-1"> <Link to="/" className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">About</Link></li>
-                    <li className="nav-item mx-0 mx-lg-1"> <Link to="/events" className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Events</Link></li>
-                    <li className="nav-item mx-0 mx-lg-1"> <Link to="/news" className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">News</Link></li>
-                    <li className="nav-item mx-0 mx-lg-1"> <Link to="/blogs" className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger">Blogs</Link></li>
-                   </ul>
-                </div>
-            </div>
-        </nav>
+        <nav className="navbar navbar-expand-lg bg-light sticky-top" id="main-nav-bar">
+    <a className="navbar-brand" href="#home">
+        <img src="../logo.svg" className="logo3 navbar-sqnlogo d-inline-block align-top"/>
+        <span className="brand-logo">MU Women in Stem</span>
+    </a>
+    <a className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <img src="../bars.svg" className="menu"/>
+    </a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+            <li className="nav-item"><a className="nav-link" data-toggle="collapse" data-target="#navbarNav" href="#home">Home</a></li>
+            <li className="nav-item"><a className="nav-link" data-toggle="collapse" data-target="#navbarNav" href="#about">News</a></li>
+            <li className="nav-item"><a className="nav-link" data-toggle="collapse" data-target="#navbarNav" href="#whatwedo">Events</a></li>
+            <li className="nav-item"><a className="nav-link" data-toggle="collapse" data-target="#navbarNav" href="#join">Blogs</a></li>
+        </ul>
+    </div>
+</nav>
         </div>
    );
 };
